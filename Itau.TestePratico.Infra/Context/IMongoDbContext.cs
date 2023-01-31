dc948b89cb07f3e74861736bdc1d94758e9cca32
Marcos Guingaro <guingarodelmarcos@gmail.com>
2023-01-31 15:23:52 -0300

    Removing infra.

--- Itau.TestePratico.Infra/Context/IMongoDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using MongoDB.Driver;
-
-namespace Itau.TestePratico.Infra.Context
-{
-    public interface IMongoDbContext
-    {
-        IMongoCollection<T> GetCollection<T>(string collection);
-    }
-}
